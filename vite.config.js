7d60f248ed3dfa7f858ff378e57d63b37a7d8d93
Jaysup29 <jayar.revis@GSOUTH.LOCAL>
2026-04-05 15:17:13 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: 'manifest.json',
-    },
 });
